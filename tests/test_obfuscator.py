5cd8882d4827a0931f19e130a56fbc823b2d718c
tudorsonycx <tudorsonycx@gmail.com>
2025-01-09 20:49:03 +0000

    Add S3 mocking and test cases for obfuscation of CSV, JSON, and Parquet files

--- tests/test_obfuscator.py
+++ tests/test_obfuscator.py
@@ -1,94 +1,52 @@
 import pytest
 from unittest.mock import patch
+from moto import mock_aws
+import boto3
 from obfuscator.obfuscator import obfuscate_file
 import pandas as pd
 from pandas.testing import assert_frame_equal
 import json
 import io
+import os
+
+
+@pytest.fixture(scope="function")
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture(scope="function")
+def s3(aws_credentials):
+    with mock_aws():
+        yield boto3.client("s3", region_name="eu-west-2")
 
 
 @pytest.fixture
-def s3_client_mock():
-    with patch("boto3.client") as mock:
-        yield mock
-
-
-def test_obfuscate_csv_file(s3_client_mock):
-    s3_client_mock.return_value.get_object.return_value = {
-        "Body": io.BytesIO(
-            b"Name,Email\nJohn Doe,john@example.com\nJane Doe,jane@example.com"
-        )
-    }
-    input_file = json.dumps(
-        {"file_to_obfuscate": "s3://bucket/test.csv", "pii_fields": ["Email"]}
+def create_bucket(s3):
+    s3.create_bucket(
+        Bucket="test-bucket",
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
     )
-    result = obfuscate_file(input_file)
-    expected = b"Name,Email\nJohn Doe,*\nJane Doe,*\n"
-    assert result == expected
 
 
-def test_obfuscate_json_file(s3_client_mock):
-    s3_client_mock.return_value.get_object.return_value = {
-        "Body": io.BytesIO(
-            b'[{"Name": "John Doe", "Email": "john@example.com"}, {"Name": "Jane Doe", "Email": "jane@example.com"}]'
-        )
-    }
-    input_file = json.dumps(
-        {"file_to_obfuscate": "s3://bucket/test.json", "pii_fields": ["Email"]}
+@pytest.fixture
+def create_files(s3, create_bucket):
+    s3.put_object(
+        Bucket="test-bucket",
+        Key="test.csv",
+        Body=b"Name,Email\nJohn Doe,john@example.com\nJane Doe,jane@example.com",
     )
-    result = obfuscate_file(input_file)
-    expected = b'[{"Name":"John Doe","Email":"*"},{"Name":"Jane Doe","Email":"*"}]'
-    assert result == expected
 
-
-def test_obfuscate_parquet_file(s3_client_mock):
-    df = pd.DataFrame(
-        {
-            "Name": ["John Doe", "Jane Doe"],
-            "Email": ["john@example.com", "jane@example.com"],
-        }
+    s3.put_object(
+        Bucket="test-bucket",
+        Key="test.json",
+        Body=b'[{"Name": "John Doe", "Email": "john@example.com"}, {"Name": "Jane Doe", "Email": "jane@example.com"}]',
     )
-    parquet_buffer = io.BytesIO()
-    df.to_parquet(parquet_buffer, index=False)
-    parquet_buffer.seek(0)
-
-    s3_client_mock.return_value.get_object.return_value = {"Body": parquet_buffer}
-    input_file = json.dumps(
-        {"file_to_obfuscate": "s3://bucket/test.parquet", "pii_fields": ["Email"]}
-    )
-    result = obfuscate_file(input_file)
-
-    result_df = pd.read_parquet(io.BytesIO(result))
-    expected_df = pd.DataFrame({"Name": ["John Doe", "Jane Doe"], "Email": ["*", "*"]})
-    assert_frame_equal(result_df, expected_df)
-
-
-def test_invalid_file_format():
-    input_file = json.dumps(
-        {"file_to_obfuscate": "s3://bucket/test.txt", "pii_fields": ["Email"]}
-    )
-    with pytest.raises(ValueError):
-        obfuscate_file(input_file)
-
-
-def test_missing_field(s3_client_mock):
-    s3_client_mock.return_value.get_object.return_value = {
-        "Body": io.BytesIO(
-            b"Name,Email\nJohn Doe,john@example.com\nJane Doe,jane@example.com"
-        )
-    }
-    input_file = json.dumps(
-        {"file_to_obfuscate": "s3://bucket/test.csv", "pii_fields": ["Phone"]}
-    )
-    with pytest.raises(KeyError):
-        obfuscate_file(input_file)
-
-    s3_client_mock.return_value.get_object.return_value = {"Body": io.BytesIO(b"[]")}
-    input_file = json.dumps(
-        {"file_to_obfuscate": "s3://bucket/test.json", "pii_fields": ["Phone"]}
-    )
-    with pytest.raises(KeyError):
-        obfuscate_file(input_file)
 
     df = pd.DataFrame(
         {
@@ -99,10 +57,61 @@ def test_missing_field(s3_client_mock):
     buf = io.BytesIO()
     df.to_parquet(buf, index=False)
     buf.seek(0)
+    s3.put_object(Bucket="test-bucket", Key="test.parquet", Body=buf)
 
-    s3_client_mock.return_value.get_object.return_value = {"Body": buf}
+
+def test_obfuscate_csv_file(create_files):
     input_file = json.dumps(
-        {"file_to_obfuscate": "s3://bucket/test.parquet", "pii_fields": ["Phone"]}
+        {"file_to_obfuscate": "s3://test-bucket/test.csv", "pii_fields": ["Email"]}
+    )
+    result = obfuscate_file(input_file)
+    expected = b"Name,Email\nJohn Doe,*\nJane Doe,*\n"
+    assert result == expected
+
+
+def test_obfuscate_json_file(create_files):
+    input_file = json.dumps(
+        {"file_to_obfuscate": "s3://test-bucket/test.json", "pii_fields": ["Email"]}
+    )
+    result = obfuscate_file(input_file)
+    expected = b'[{"Name":"John Doe","Email":"*"},{"Name":"Jane Doe","Email":"*"}]'
+    assert result == expected
+
+
+def test_obfuscate_parquet_file(create_files):
+    input_file = json.dumps(
+        {"file_to_obfuscate": "s3://test-bucket/test.parquet", "pii_fields": ["Email"]}
+    )
+    result = obfuscate_file(input_file)
+
+    result_df = pd.read_parquet(io.BytesIO(result))
+    expected_df = pd.DataFrame({"Name": ["John Doe", "Jane Doe"], "Email": ["*", "*"]})
+    assert_frame_equal(result_df, expected_df)
+
+
+def test_invalid_file_format():
+    input_file = json.dumps(
+        {"file_to_obfuscate": "s3://test-bucket/test.txt", "pii_fields": ["Email"]}
+    )
+    with pytest.raises(ValueError):
+        obfuscate_file(input_file)
+
+
+def test_missing_field(create_files):
+    input_file = json.dumps(
+        {"file_to_obfuscate": "s3://test-bucket/test.csv", "pii_fields": ["Phone"]}
+    )
+    with pytest.raises(KeyError):
+        obfuscate_file(input_file)
+
+    input_file = json.dumps(
+        {"file_to_obfuscate": "s3://test-bucket/test.json", "pii_fields": ["Phone"]}
+    )
+    with pytest.raises(KeyError):
+        obfuscate_file(input_file)
+
+    input_file = json.dumps(
+        {"file_to_obfuscate": "s3://test-bucket/test.parquet", "pii_fields": ["Phone"]}
     )
     with pytest.raises(KeyError):
         obfuscate_file(input_file)
