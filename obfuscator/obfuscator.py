7d267f14ff0fb1180d82ff0e3d289c3117e53af8
tudorsonycx <tudorsonycx@gmail.com>
2025-01-09 19:36:41 +0000

    Enhance obfuscation function to support JSON and Parquet file formats

--- obfuscator/obfuscator.py
+++ obfuscator/obfuscator.py
@@ -8,10 +8,14 @@ import json
 def obfuscate_file(input_file: str) -> bytes:
     input_file = json.loads(input_file)
     file_to_obfuscate = input_file["file_to_obfuscate"]
-    if not (re.match(r"^s3://[^/]+/[^/]+.(?:(?:csv))$", file_to_obfuscate)):
+    if not (
+        re.match(
+            r"^s3://[^/]+/[^/]+.(?:(?:csv)|(?:json)|(?:parquet))$", file_to_obfuscate
+        )
+    ):
         raise ValueError(
             "The file_to_obfuscate must be in the format 's3://<bucket>/<key>.<extension>'"
-            " where extension is 'csv'"
+            " where extension is one of 'csv', 'json', or 'parquet'"
         )
 
     bucket, file_key = file_to_obfuscate.split("s3://")[1].split("/", 1)
@@ -32,4 +36,22 @@ def obfuscate_file(input_file: str) -> bytes:
             df[field] = "*"
         df.to_csv(buf, index=False)
 
+    elif file_extension == "json":
+        df = pd.read_json(obj_body, orient="records")
+        for field in pii_fields:
+            if field not in df.columns:
+                raise KeyError(f"Field '{field}' not found in the JSON file")
+            df[field] = "*"
+        df.to_json(buf, orient="records", index=False)
+
+    elif file_extension == "parquet":
+        parquet_file = io.BytesIO(obj_body.read())
+        df = pd.read_parquet(parquet_file)
+        for field in pii_fields:
+            if field not in df.columns:
+                raise KeyError(f"Field '{field}' not found in the parquet file")
+            df[field] = "*"
+        df.to_parquet(buf, index=False)
+
     return buf.getvalue()
+
