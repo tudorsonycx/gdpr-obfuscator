b38a85b82ade821b646cbf746f0b3f3c493efe30
tudorsonycx <tudorsonycx@gmail.com>
2025-01-10 23:13:27 +0000

    Update import statement for obfuscator module

--- tests/test_obfuscator.py
+++ tests/test_obfuscator.py
@@ -1,7 +1,7 @@
 import pytest
 from moto import mock_aws
 import boto3
-from obfuscator.obfuscator import obfuscate_file
+from gdpr_obfuscator import obfuscate_file
 import pandas as pd
 from pandas.testing import assert_frame_equal
 import json
